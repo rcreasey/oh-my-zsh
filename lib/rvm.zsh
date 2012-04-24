afb5d9114daab0936b268102910aeda343753014
Ryan C. Creasey <rcreasey@blizzard.com>
2012-04-23 17:39:58 -0700

    chomping rvm version.

--- lib/rvm.zsh
+++ lib/rvm.zsh
@@ -1,7 +1,7 @@
 # get the name of the branch we are on
 function rvm_prompt_info() {
-  ruby_version=$(~/.rvm/bin/rvm-prompt 2> /dev/null) || return
-  echo "($ruby_version)"
+  ruby_version=$(~/.rvm/bin/rvm-prompt 2> /dev/null | sed -e 's/ruby\-//') || return
+  echo "$ruby_version"
 }
 
 
